e1937dcfb486f9ff1a2fc1820b064319f5ffea49
HenryZ94264 <henryz99@foxmail.com>
2023-03-12 18:33:41 +0800

    add some resources

---
+++ inc/include_set.h
@@ -0,0 +1,17 @@
+#ifndef __MYINCLUDESET__
+#define __MYINCLUDESET__
+
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <errno.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+
+#endif
---
+++ inc/mySocket.h
@@ -0,0 +1,20 @@
+#ifndef __MYSOCKET__
+#define __MYSOCKET__
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <iostream>
+
+#define LOG(x) std::cout << x << std::endl
+
+enum SOCKET_FAILED_TYPE :int
+{
+    SOCKET = -1, BIND = -2, LISTEN = -3
+};
+
+int getSocket(const char*, const int&, bool =false);
+
+
+
+#endif
